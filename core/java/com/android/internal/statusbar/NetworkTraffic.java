94d155312f1ed5c837fcd90bd7a25a4e68a1c1b1
leo <leo>
2026-02-08 15:51:11 +0800

    NetworkTraffic: fixup the display style of Network Traffic Indicator
    modify the display style of Network traffic indicator on status bar & make it display on the status bar of Lockscreen
    
    Change-Id: I366892b38a537ddfb8076f3a60b1a2aad75d6e62
    
    fixup! NetworkTraffic: enable Network Traffic Indicator on QS heard
    
    Change-Id: I02a44713b316a13697e36b70fa15d6abf4639bb8

--- core/java/com/android/internal/statusbar/NetworkTraffic.java
+++ core/java/com/android/internal/statusbar/NetworkTraffic.java
@@ -33,6 +33,10 @@ import android.widget.TextView;
 
 import com.android.internal.R;
 
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.RelativeSizeSpan;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.stream.Stream;
@@ -83,7 +87,7 @@ public class NetworkTraffic extends TextView {
     private boolean mAutoHide;
     private long mAutoHideThreshold;
     private int mUnits;
-    private int mIconTint = Color.WHITE;
+    protected int mIconTint = Color.WHITE;
 
     // Network tracking related variables
     private ConnectivityManager mConnectivityManager;
@@ -111,6 +115,8 @@ public class NetworkTraffic extends TextView {
         setTypeface(Typeface.create(resources.getString(
                 com.android.internal.R.string.config_headlineFontFamily), Typeface.BOLD));
 
+        setLineSpacing(0.82f, 0.82f);
+        
         mNetworkTrafficIsVisible = false;
 
         mConnectivityManager = mContext.getSystemService(ConnectivityManager.class);
@@ -215,6 +221,18 @@ public class NetworkTraffic extends TextView {
                     if (!output.toString().contentEquals(getText())) {
                         setTextSize(TypedValue.COMPLEX_UNIT_PX, (float) mTextSize);
                         setText(output.toString());
+
+                        int currentSize = getResources().getDimensionPixelSize(R.dimen.net_traffic_text_size);                        
+                        setTextSize(TypedValue.COMPLEX_UNIT_PX, currentSize);
+                        String textContent = output.toString();
+                        SpannableString span = new SpannableString(textContent);
+                        int lineIndex = textContent.indexOf("\n");
+                        if (lineIndex != -1) {
+                            span.setSpan(new RelativeSizeSpan(1.32f), 0, lineIndex, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);                            
+                            span.setSpan(new RelativeSizeSpan(0.88f), lineIndex + 1, textContent.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                        }                        
+                        setText(span);
+                        setTextColor(mIconTint);
                     }
                     setVisibility(VISIBLE);
                 }
@@ -354,20 +372,28 @@ public class NetworkTraffic extends TextView {
     private final CustomStatusBarItem.VisibilityReceiver mVisibilityReceiver =
             new CustomStatusBarItem.VisibilityReceiver() {
                 public void onVisibilityChanged(boolean isVisible) {
-                    if (mNetworkTrafficIsVisible != isVisible) {
-                        mNetworkTrafficIsVisible = isVisible;
-                        updateViewState();
-                    }
+                    setTrafficVisible(isVisible);
                 }
             };
+
+    protected void setTrafficVisible(boolean isVisible) {
+        if (mNetworkTrafficIsVisible != isVisible) {
+            mNetworkTrafficIsVisible = isVisible;
+            updateViewState();
+        }
+    }
 
     @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
 
         CustomStatusBarItem.Manager manager = CustomStatusBarItem.findManager((View) this);
-        manager.addDarkReceiver(mDarkReceiver);
-        manager.addVisibilityReceiver(mVisibilityReceiver);
+        if (manager != null) {
+            manager.addDarkReceiver(mDarkReceiver);
+            manager.addVisibilityReceiver(mVisibilityReceiver);
+        } else {
+            setTrafficVisible(true);
+        }
 
         mObserver.observe();
         updateSettings();
@@ -444,7 +470,7 @@ public class NetworkTraffic extends TextView {
         updateViewState();
     }
 
-    private void updateViewState() {
+    protected void updateViewState() {
         mTrafficHandler.sendEmptyMessage(MESSAGE_TYPE_UPDATE_VIEW);
     }
 
